e21ad926f0f9688c84fe6128f171309c60cb8728
jakeandshake <jmagri1@uncc.edu>
2023-07-05 20:57:00 -0400

    Finished game. Need to follow up with comments.

--- PongGame/Ball.js
+++ PongGame/Ball.js
@@ -25,18 +25,28 @@ export default class Ball {
         this.y = 50
         this.direction = { x: 0}
         while (
-            Math.abs(this.direction.x) <= .2 || 
-            Math.abs(this.direction.x) >= .9
+            Math.abs(this.direction.x) <= 0.2 || 
+            Math.abs(this.direction.x) >= 0.9
         ) {
             const heading = randomNumberBetween(0, 2 * Math.PI)
             this.direction = { x: Math.cos(heading), y: Math.sin(heading)}
         }
-        console.log(this.direction)
+        this.velocity = INITIAL_VELOCITY
     }
 
-    update(delta) {
-    this.x = 5
-    this.y = 15
+    update(delta, paddleRects) {
+    this.x += this.direction.x * this.velocity * delta
+    this.y += this.direction.y * this.velocity * delta
+    this.velocity += VELOCITY_INCREASE * delta
+    const rect = this.rect()
+    // checks if ball is off top or bottom of the screen.
+    if (rect.bottom >= window.innerHeight || rect.top <= 0) {
+        this.direction.y *= -1
+    }
+    // checks if any of our rectangles(rect) had a collision with the ball.
+    if (paddleRects.some(r => isCollision(r, rect))) {
+        this.direction.x *= -1
+    }
     }
 }
 
